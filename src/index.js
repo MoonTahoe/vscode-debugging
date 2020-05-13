0de95fd19dd1b08bd6b41433cd6899dbcb85b735
MoonTahoe <alex@moonhighway.com>
2020-05-13 12:03:38 -0700

    Debugging with babel-node

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require("apollo-server");
+import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
   type Query {
@@ -8,7 +8,9 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    gnar: () => "gnarly!!!",
+    gnar() {
+      return "gnarly!!!";
+    },
   },
 };
 
